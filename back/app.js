8f7ec9a3aafa5fb57a638e4298f638caeb9282c8
Seojunhyeong2003 <seojunhyeong2003@gmail.com>
2023-05-16 02:49:34 +0900

    dfs

---
+++ back/app.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello node');
+})
+
+app.listen(3000, () => console.log('3000 포트에서 대기중'));
